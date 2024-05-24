9c50c894076c31c80d53f2b21a2c15f5286bf241
ajharul64 <ajharul.islam@barclays.com>
2024-05-24 10:33:41 +0100

    edits

--- routes/shows.js
+++ routes/shows.js
@@ -5,6 +5,8 @@ const { Show } = require('../models')
 router.use(express.json())
 router.use(express.urlencoded({extended: true}));
 
+const { check, validationResult } = require('express-validator');
+
 router.get('/', async (req,res)=>{
     const shows = await Show.findAll()
     res.json(shows)
@@ -16,8 +18,45 @@ router.get('/:id', async (req,res)=>{
 });
 
 router.get('/:genre', async (req,res)=>{
-    const shows = await Show.findAll({where:{genre: req.params.genre.toLowerCase()}})
+    const shows = await Show.findAll(req.body,{where:{genre: req.params.genre}})
     res.json(shows)
+});
+
+router.put('/:id/:rating', [check("rating").notEmpty().isNumeric()], async (req,res)=>{
+    
+
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+        const show = await Show.findByPk(req.params.id)
+        show.rating  = req.body.push(req.params.rating)
+        res.json(show.rating)
+    } 
+    else {
+        res.json({
+            error: result.array(),
+        });
+    }
+});
+
+router.put('/:id/:status',[check("status").notEmpty(),check("status").isLength({min:5, max: 25})], async (req,res)=>{
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+        const show = await Show.findByPk(req.params.id)
+        show.status = req.body.push(req.params.status);
+        res.json(show.status)
+    } 
+    else {
+        res.json({
+            error: result.array(),
+        });
+    }
+});
+
+
+
+router.delete('/:id', async (req,res)=>{
+    const deletedshow = await Show.destroy(req.body, {where: {id:req.params.id}})
+    res.json(show)
 });
 
 module.exports = router;
